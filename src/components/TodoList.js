a3c8cd7680d75048aa98e0effc7a77bcd87ddd90
Victoria Sapronova <victoriasapronova@Victorias-MBP.fios-router.home>
2021-01-29 15:12:09 -0500

    Added "Add Todo"

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,29 +1,65 @@
 import React, { useState } from "react";
 import todosData from "../todosData";
 import TodoItem from "../components/TodoItem";
+import shortid from "shortid";
 
 const TodoList = () => {
   const [filteredTodos, setFilteredTodos] = useState(todosData);
+  const [todo, setTodo] = useState("");
+  const [searchText, setSearchText] = useState("")
 
-  const handleChange = (event) => {
-    let searchText = event.target.value.toLowerCase();
+  const searchTextChanged = (event) => {
+    setSearchText(event.target.value)
+    updateFilteredTodos(event.target.value)
+  };
+
+  const updateFilteredTodos = newSearchText => {
+    let searchTextLower = newSearchText.toLowerCase();
     const filtered = todosData.filter((item) =>
-      item.description.toLowerCase().startsWith(searchText)
+      item.description.toLowerCase().startsWith(searchTextLower)
     );
     setFilteredTodos(filtered);
+  }
+
+  const todoChanged = (event) => {
+    setTodo(event.target.value);
   };
+
+  const addTodo = () => {
+    todosData.unshift({
+      id: shortid.generate(),
+      description: todo,
+      completeStatus: false
+    })
+    updateFilteredTodos(searchText)
+    setTodo("");
+  };
+
   const todoItems = filteredTodos.map((item) => (
     <TodoItem key={item.id} item={item} />
   ));
+
   return (
     <div className="todo-list">
       <input
         className="todo-search"
         type="text"
+        value={searchText}
         placeholder="Search"
-        onChange={handleChange}
+        onChange={searchTextChanged}
       />
-
+      <div className="todo-add-container">
+        <input
+          className="todo-search"
+          type="text"
+          value={todo}
+          placeholder="Add todo"
+          onChange={todoChanged}
+        />
+        <button className="todo-add-button" onClick={addTodo}>
+          Add
+        </button>
+      </div>
       {todoItems}
     </div>
   );
